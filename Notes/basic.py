19cb84630bbffc01211d3faba4401a9ff84eb2e0
Kang <mminjg@gmail.com>
2021-01-05 15:16:29 +0900

    Create basic.py

---
+++ Notes/basic.py
@@ -0,0 +1,38 @@
+# sys를 이용한 input
+import sys
+
+# 정수 입력
+n = int(sys.stdin.readline())
+# 문자열 입력, 마지막 \n 제거 필요
+data = sys.stdin.readline().rstrip()
+# 정수 리스트
+arr = list(map(int, sys.stdin.readline().split()))
+
+
+# 1차원 리스트 0으로 초기화 [0, 0,  ... , 0]
+arr = [0] * n
+# 1차원 리스트 i값으로 초기화 [0, 1, 2, 3, .. 9]
+arr = [ i for i in range(10)]   # == list(range(10), list(range(0, 10)
+# 1차원 리스트에 입력값 넣기
+for i in range(n):
+    arr[i] = int(sys.stdin.readline())  # append로 초기화 하는 것보다 빠르다
+
+# 2차원 리스트 초기화 [[0, 0], [0, 0], [0, 0]]
+arr = [[0] * 2 for _ in range(3)]   # == [[0 for j in range(2)] for i in range(3)]
+
+
+# itertools
+from itertools import permutations
+from itertools import combinations
+from itertools import product
+from itertools import combinations_with_replacement
+data = ['a','b','c']
+
+# 모든 순열 구하기
+result = list(permutations(data,3))
+# 모든 조합 구하기
+result = list(combinations(data, 3))
+# 중복 순열 구하기
+result = list(product(data, repeat=2)) # 2개 뽑는 모든 순열 (중복 허용)
+# 중복 조합 구하기
+result = list(combinations_with_replacement(data, 2)) # 2개 뽑는 모든 조합 (중복 허용)
