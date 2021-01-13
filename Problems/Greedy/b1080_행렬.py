0eec7dfed69bc06c2d7d5c5f5299b96182a38062
Kang <mminjg@gmail.com>
2021-01-13 14:41:07 +0900

    Update comment

--- Problems/Greedy/b1080_행렬.py
+++ Problems/Greedy/b1080_행렬.py
@@ -1,6 +1,7 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
+# 행렬 a, b n * m 초기화
 a = [[0] * m for _ in range(n)]
 b = [[0] * m for _ in range(n)]
 
@@ -10,12 +11,14 @@ for i in range(n):
 for i in range(n):
     b[i] = list(sys.stdin.readline().rstrip())
 
+# 맨 왼쪽 오른쪽 값은 한번 확인하면 더이상 고려하지 않아도 된다.
 count = 0
 for i in range(n-2):
     for j in range(m-2):
-        if a[i][j] == b[i][j]:
+        if a[i][j] == b[i][j]:  # 같다면 다음으로
             continue
-        count += 1  # 맨 왼쪽 오른쪽 값이 다르다면 바꿈
+        # 다르다면 3*3크기의 부분행렬에 있는 모든 원소를 뒤집어야 한다.
+        count += 1
         for k in range(i, i+3):
             for l in range(j, j+3):
                 if a[k][l] == '0':
@@ -23,6 +26,7 @@ for i in range(n-2):
                 else:
                     a[k][l] = '0'
 
+# 행렬 a와 행렬 b가 같은지 확인한다.
 for i in range(n):
     for j in range(m):
         if a[i][j] != b[i][j]:
--- Problems/Greedy/b4796_캠핑.py
+++ Problems/Greedy/b4796_캠핑.py
@@ -3,15 +3,15 @@ import sys
 case = 0
 while True:
     l, p, v = map(int, sys.stdin.readline().split())
-    if l == 0:
+    if l == 0:  # case가 더이상 없으면 종료한다.
         break
 
     case += 1
     days = 0
 
-    days += v // p * l
+    days += v // p * l  # v일 휴가 // 연속하는 p일 * l일 동안 사용
 
-    if v % p <= l:
+    if v % p <= l:  # v일 휴가를 p로 나누고 남은 휴가가 l일 보다 작을 때 더해준다
         days += v % p
     else:
         days += l
