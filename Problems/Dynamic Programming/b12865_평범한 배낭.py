cea306e1e7148c1b51b78fba2c6db880a137d674
Kang <mminjg@gmail.com>
2021-01-27 21:48:17 +0900

    Update comments

--- Problems/Dynamic Programming/b12865_평범한 배낭.py
+++ Problems/Dynamic Programming/b12865_평범한 배낭.py
@@ -2,11 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-
+# dp[i][j] i번째 물건까지 고려했을 때의 가치의 최댓값, 배낭의 무게 j 일 때
 dp = [[0] * (k + 1) for _ in range(n + 1)]
 v = [0] * n
 w = [0] * n
 
+# 입력
 for i in range(n):
     w[i], v[i] = map(int, input().split())
 
@@ -14,6 +15,7 @@ mx = 0
 for i in range(1, n + 1):
     for j in range(1, k + 1):
         if w[i - 1] <= j:   # 현재 물건을 배낭에 넣을 수 있는 경우
+            # w무게를 뺀 배낭의 가치 + 현재 물건 가치와 / 현재 물건을 넣지 않을 때의 가치 중 큰 값
             dp[i][j] = max(v[i - 1] + dp[i - 1][j - w[i - 1]], dp[i - 1][j])
         else:   # 현재 물건을 배낭에 넣을 수 없을 경우
             dp[i][j] = dp[i - 1][j]
--- Problems/Dynamic Programming/b14494_다이나믹이 뭐예요.py
+++ Problems/Dynamic Programming/b14494_다이나믹이 뭐예요.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 dp = [[0] * (m + 1) for _ in range(n + 1)]
 
+# 맨 위의 행과 왼쪽 열의 경우의 수는 1이다.
 for i in range(1, n + 1):
     dp[i][1] = 1
 
@@ -12,6 +13,7 @@ for j in range(1, m + 1):
 
 for i in range(2, n + 1):
     for j in range(2, m + 1):
+        # →, ↓, ↘ 에서 오는 경우의 수를 모두 더한다.
         dp[i][j] = (dp[i-1][j] + dp[i][j-1] + dp[i-1][j-1]) % 1000000007
 
 print(dp[n][m])
