734fab12e0ae3edf6a073937bd1c746e3b122259
Kang <mminjg@gmail.com>
2021-02-16 17:28:36 +0900

    Update comments

--- Problems/Shortest Path/b10159_저울.py
+++ Problems/Shortest Path/b10159_저울.py
@@ -6,6 +6,7 @@ n = int(input())
 m = int(input())
 graph = [[INF]*(n+1) for _ in range(n+1)]
 
+# 플로이드 워셜
 for a in range(1, n+1):
     for b in range(1, n+1):
         if a == b:
@@ -18,11 +19,18 @@ for _ in range(m):
 for k in range(1, n+1):
     for a in range(1, n+1):
         for b in range(1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
 for i in range(1, n+1):
     cnt = 0
     for j in range(1, n+1):
+        # 물건 i에서 j로, 물건 j에서 i로의 경로가 모두 없는 경우 cnt 증가
         if graph[i][j] == INF and graph[j][i] == INF:
             cnt += 1
     print(cnt)
+
+# [2] > [4] 라고 주어진 경우, 방향 그래프로 나타내면 [2]가 [4]를 가리킨다고 생각할 수 있다.
+# 즉, 무게가 무거운 물건이 무게가 작은 물건을 가리키는 방향그래프로 나타낼 수 있다.
+# 경로로 생각하면, 물건 A에서 물건 B로의 경로가 있다면 물건 A와 물건 B의 비교가 가능한 것이다.
+# 플로이드 워셜을 진행한후 물건 i와 물건 j가 비교가 가능한 지 판단하려면 graph[i][j]와 graph[i][j]를 확인하면 된다.
+# 비교 결과를 알 수 없는 경우는 graph[i][j]와 graph[j][i]가 모두 INF, 즉 경로가 없는 경우이다.
--- Problems/Stack, Queue, Deque/b11279_최대 힙.py
+++ Problems/Stack, Queue, Deque/b11279_최대 힙.py
@@ -7,9 +7,11 @@ n = int(input())
 for _ in range(n):
     x = int(input())
     if x != 0:
+        # 파이썬의 기본 힙은 최소힙, 따라서 -로 부호 바꾸어서 힙에 넣어준다.
         heapq.heappush(q, -x)
     else:
         if q:
+            # 다시 부호를 바꾸어 출력한다.
             print(-heapq.heappop(q))
         else:
             print(0)
