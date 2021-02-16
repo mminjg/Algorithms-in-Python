bdedb4c032f883b63211d68ba6be5905822b286a
Kang <mminjg@gmail.com>
2021-02-16 17:29:09 +0900

    Update comments

--- Problems/Shortest Path/b1753_최단 경로.py
+++ Problems/Shortest Path/b1753_최단 경로.py
@@ -9,20 +9,26 @@ distance = [INF] * (V+1)
 
 k = int(input())
 for _ in range(E):
+    # u에서 v로 가는 가중치 w
     u, v, w = map(int, input().split())
     graph[u].append((v, w))
 
+# 다익스트라 진행
 def dijkstra(start):
     q = []
+    # 시작 노드의 최단 경로는 0
     heapq.heappush(q, (0, start))
     distance[start] = 0
 
     while q:
         dist, now = heapq.heappop(q)
+        # 이미 처리된 적 있는 경우 continue
         if distance[now] < dist:
             continue
+        # 현재 노드와 인접한 노드 확인
         for i in graph[now]:
             cost = dist + i[1]
+            # 현재 노드를 거쳐 이동하는 경우가 거리가 더 짧은 경우
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
