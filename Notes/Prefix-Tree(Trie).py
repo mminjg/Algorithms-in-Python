08b22c626f51ac6a0ddfb37d21fa75fceccf5e37
Kang <mminjg@gmail.com>
2021-02-19 03:17:05 +0900

    Create Prefix-Tree(Trie).py

---
+++ Notes/Prefix-Tree(Trie).py
@@ -0,0 +1,24 @@
+class Trie:
+    head = {}
+
+    def add(self, word):
+        cur = self.head
+        for ch in word:
+            if ch not in cur:
+                cur[ch] = {}
+            cur = cur[ch]
+        # 마지막 노드
+        cur['*'] = True
+
+    def search(self, word):
+        cur = self.head
+        for ch in word:
+            if ch not in cur:
+                return False
+            cur = cur[ch]
+        if '*' is cur:
+            return True
+        else:
+            return False
+
+dict = Trie()
