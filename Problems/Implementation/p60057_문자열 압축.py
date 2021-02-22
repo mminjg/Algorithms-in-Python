147176a850ea5c0ea01153507f21b721c29f1d83
Kang <mminjg@gmail.com>
2021-02-22 13:25:22 +0900

    Update comment

--- Problems/Implementation/p60057_문자열 압축.py
+++ Problems/Implementation/p60057_문자열 압축.py
@@ -1,15 +1,18 @@
 def solution(s):
     n = len(s)
     result = 1001
+    # i 단위로 자른다.
     for i in range(1, n+1):
-        tmp = ''
-        cnt = 1
-        before_c = s[:i]
+        tmp = ''    # 단위가 i 일 때 압축한 문자열
+        cnt = 1     # 압축할 때 숫자 카운트
+        before_c = s[:i]    # 중복 확인을 위한 before
+        # i부터 i 단위로 자름
         for j in range(i, n, i):
             c = s[j:j+i]
-            print(before_c, c)
+            # 전과 중복되는 경우
             if before_c == c:
                 cnt += 1
+            # 전과 중복되지 않은 경우
             else:
                 if cnt == 1:
                     tmp += before_c
@@ -21,6 +24,6 @@ def solution(s):
             tmp += before_c
         else:
             tmp += (str(cnt)+before_c)
-        print(tmp)
+        # 가장 짧은 길이의 값을 result로
         result = min(result, len(tmp))
     return result
